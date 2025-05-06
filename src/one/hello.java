910b6f66d39459da708cbd0974fbb950dc00c475
Creambell <hosunjerry@naver.com>
2025-05-06 23:11:04 +0900

    Initial commit

---
+++ src/one/hello.java
@@ -0,0 +1,15 @@
+package com.kh.frist;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,Akrasia!");
+    }
+    /*
+        클래스와 메소드
+        메소드 변수는 소문자지만 메소드에는 소카로가 붙음
+        클래스는 대문자시작 패키지는 소문자시작
+        모든 코드는 클래스 안으로
+        HelloWorld , String , System 은 클래스
+        중가로를 열어서 main은 정의
+    */
+}
